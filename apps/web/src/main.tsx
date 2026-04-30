cfa66d95e58810cf6f9339c9a2cdc068a8ce5029
demonurgo <127979907+demonurgo@users.noreply.github.com>
2026-04-30 09:36:26 -0300

    feat: apply Balaio brand font to headers

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "@fontsource/caveat-brush/400.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
